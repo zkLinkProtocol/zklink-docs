e3250f41efbe3783e5729f77e12f2ab451fc285c
tyral <tyral@zklinik.org>
2021-06-18 02:16:39 +0800

    chore: pm2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name        : "zklink-docs",
+    script      : "npm run serve",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+       "NODE_ENV": "production"
+    }
+  }]
+}
